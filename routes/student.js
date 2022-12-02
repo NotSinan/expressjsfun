7abb35db3a1c098e0cb1dd25e6721dbadff4548e
Sinan <haxsmith77@gmail.com>
2022-12-02 20:38:40 +1100

    Added new routes.

--- routes/student.js
+++ routes/student.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const Student = require('../models/student.model');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Student route works');
 });
+
+router.get('/add', async (req, res, next) => {
+
+    const newStudent = await Student.create({
+        studentId: 2,
+        studentFirstName: "Yo",
+        studentLastName: "Woo"
+
+    })
+
+    res.send({student: newStudent});
+});
+
+
+
 
 module.exports = router;
